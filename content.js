423db9cdfbb8c2df32972090b33efe146ef17b5d
sss <glee.fun@outlook.jp>
2024-04-30 12:26:36 +0900

    add: カテゴリー絞り込み機能

--- content.js
+++ content.js
@@ -12,6 +12,7 @@ var observer = new MutationObserver(function (mutations) {
             newDiv.textContent = "絞り込み：";
             var input = document.createElement("input");
             input.type = "text";
+            input.addEventListener("input", filterList);
             newDiv.appendChild(input);
             element.parentNode.insertBefore(newDiv, element.nextSibling);
           }
@@ -27,6 +28,20 @@ var observer = new MutationObserver(function (mutations) {
       });
     }
   });
+
+  function filterList(event) {
+    var filter = event.target.value.toUpperCase();
+    var ul = document.querySelector(".style-xntn0");
+    var li = ul.getElementsByTagName("li");
+    for (var i = 0; i < li.length; i++) {
+      var txtValue = li[i].textContent || li[i].innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
