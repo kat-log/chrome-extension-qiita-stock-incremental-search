894d4738df1d0f58315cb83ca0a8f3cd7d8f62f0
sss <glee.fun@outlook.jp>
2024-04-30 13:28:33 +0900

    fix: アイコンクリック時の文章

--- popup.js
+++ popup.js
@@ -16,8 +16,10 @@ function checkElement() {
 function handleResult(result) {
   var statusElement = document.getElementById("status");
   if (result && result[0].result) {
-    statusElement.textContent = "style-ss550tが見つかりました！";
+    statusElement.textContent =
+      "ストックのポップアップ（style-ss550t）が見つかりました！フォームが表示されてない場合、ページが完全に読み込まれてから再度ポップアップしてみてください🙇🙇";
   } else {
-    statusElement.textContent = "style-ss550tが見つかりませんでした！";
+    statusElement.textContent =
+      "ページが完全に読み込まれたらストックのポップアップを表示してみてください（style-ss550tが見つかりませんでした）";
   }
 }
